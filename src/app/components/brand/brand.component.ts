73e497b4f9ea64f6f6a328e1d793e11a3637d766
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-09-17 21:36:00 +0300

    17/08 Part/2

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
   styleUrls: ['./brand.component.css']
 })
-export class BrandComponent {
+export class BrandComponent implements OnInit {
+  brands: Brand[] = []
+constructor(private brandServices: BrandService){
+}
+  ngOnInit(): void {
+    this.getBrands();
+  }
+  setCurrentBrand(brand:Brand){
+    console.log(brand.brandName)
+  }
+  getBrands(){
+    this.brandServices.getBrands().subscribe((response)=>{
+      this.brands = response.data;
+    })
+  }
+ 
 
 }
--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModule } from '../models/listResponseModel';
+import { Brand } from '../models/brand';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
 
-  constructor() { }
+  apiUrl="https://localhost:44385/api/Brands"
+  constructor(private httpClient: HttpClient) {
+
+   }
+   getBrands(): Observable<ListResponseModule<Brand>>{
+   let newPath = this.apiUrl + "/getall";
+      return this.httpClient.get<ListResponseModule<Brand>>(newPath);
+   }
 }
